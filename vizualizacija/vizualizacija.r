08941558e1b6f01a0d2278f96b24c6b4ab5a2772
Enja Erker <enja.erker@gmail.com>
2020-05-14 22:26:34 +0200

    Popravki

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -16,7 +16,6 @@ t1 <- ggplot(gostota_zag, aes(x="", y=Število, fill=Skupina)) + geom_bar(stat="
   labs(x = NULL, y = NULL, fill = NULL, title = "Gostota prebivalstva v Sloveniji leta 2015") +
   theme_classic() + theme(axis.line = element_blank(),axis.text = element_blank(),axis.ticks = element_blank(),plot.title = element_text(hjust = 0.5, color = "#666666")) +
   geom_text(aes(label = paste(round(Število / sum(Število) * 100, 1), "%"), x = 1.3),position = position_stack(vjust = 0.5))
-plot(t1)
 
 #graficni prikaz kriticnosti brezposelnosti v Sloveniji (izračun kritičnih in nekritičnih, nov data table s podatki, izris tortnega diagrama)
 st_kriticnih <- sum(brezposelni$'kriticnost' == 'kritična')
@@ -30,7 +29,6 @@ t2 <- ggplot(kriticnost, aes(x="", y=Število, fill=Kritičnost)) + geom_bar(sta
   labs(x = NULL, y = NULL, fill = NULL, title = "Kritičnost brezposelnosti v Sloveniji leta 2015") +
   theme_classic() + theme(axis.line = element_blank(),axis.text = element_blank(),axis.ticks = element_blank(),plot.title = element_text(hjust = 0.5, color = "#666666")) +
   geom_text(aes(label = paste(round(Število / sum(Število) * 100, 1), "%"), x = 1.3),position = position_stack(vjust = 0.5))
-plot(t2)
 
 #graficni prikaz povprečne mesečne neto plače (indeks) v Sloveniji (glede na delitev na tri skupine) ((izračun 'razredov', nov data table s podatki, izris tortnega diagrama))
 prva <- sum(placa_indeks$'razred' == 'nizka')
@@ -45,7 +43,6 @@ t3 <- ggplot(place, aes(x="", y=Število, fill=Višina)) + geom_bar(stat="identi
   labs(x = NULL, y = NULL, fill = NULL, title = "Povprečne mesečne neto plače na ravni občin glede na slovensko povprečje leta 2015 po skupinah") +
   theme_classic() + theme(axis.line = element_blank(),axis.text = element_blank(),axis.ticks = element_blank(),plot.title = element_text(hjust = 0.5, color = "#666666"))+
   geom_text(aes(label = paste(round(Število / sum(Število) * 100, 1), "%"), x = 1.3),position = position_stack(vjust = 0.5))
-plot(t3)
 
 #NAPELJAVE, GRAFI
 
@@ -53,19 +50,16 @@ plot(t3)
 g1 <- ggplot(leta.skupine.vod) + aes(x=leto, y=povprecje, color=skupina) + geom_line() + geom_point() +
   labs(x="Leto",y="Povprečno število stanovanj z vodovodom",title="Število vodovodnih napeljav glede na gostoto poselitve, 2002 - 2010", color = "Gostota poselitve") +
   theme(legend.position = "bottom") 
-plot(g1)
 
 #graficni prikaz kanalizacije in gostote
 g2 <- ggplot(leta.skupine.kan) + aes(x=leto, y=povprecje, color=skupina) + geom_line() + geom_point() +
   labs(x="Leto",y="Povprečno število stanovanj s kanalizacijo",title="Število napeljav kanalizacije glede na gostoto poselitve, 2002 - 2010", color = "Gostota poselitve") +
   theme(legend.position = "bottom") 
-plot(g2)
 
 #graficni prikaz plac in ogrevanja
 g3 <- ggplot(leta.razredi) + aes(x=leto, y=povprecje, color=razred) + geom_line() + geom_point() +
   labs(x="Leto",y="Povprečno število stanovanj z ogrevanjem",title="Število ogrevalnih napeljav in plače prebivalstva, 2002 - 2010", color = "Povprečna višina plač") +
   theme(legend.position = "bottom") 
-plot(g3)
 
 #POMANKLJIVOSTI, TORTNI PRIKAZ
 
@@ -83,52 +77,37 @@ tr <- ggplot(pomankljivi, aes(x="", y=Število, fill=Stopnja)) + geom_bar(stat="
   labs(x = NULL, y = NULL, fill = NULL, title = "Delež občin s pomankljivo opremljenostjo stanovanj leta 2015") +
   theme_classic() + theme(axis.line = element_blank(),axis.text = element_blank(),axis.ticks = element_blank(),plot.title = element_text(hjust = 0.5, color = "#666666")) +
   geom_text(aes(label = paste(round(Število / sum(Število) * 100, 1), "%"), x = 1.3),position = position_stack(vjust = 0.5))
-plot(tr)
 
 #KAZALNIKI, ZEMLJEVIDI
 
 #gostota prebivalstva
 z1 <- tm_shape(merge(obcine, gostota_z, by.x="OB_UIME", by.y="Obcina")) + tm_polygons("vrednost", title="Gostota naseljenosti v preb/km^2") +
   tm_layout(title="Gostota prebivalstva Slovenije po občinah leta 2015", legend.position=c(0.82,0.005), legend.height=0.7, title.size=1.5)
-z1
 
 #place
 z2 <- tm_shape(merge(obcine, placa_indeks_z, by.x="OB_UIME", by.y="Obcina")) + tm_polygons("vrednost", title="Vrednost indeksa") + 
   tm_layout(title="Povprečne mesečne neto plače glede na slovensko povprečje (indeks = 100) po občinah leta 2015",legend.position=c(0.82,0.005), legend.height=0.7, title.size=1.5) 
-z2
 
 #brezposelni
 z3 <- tm_shape(merge(obcine, brezposelni_z, by.x="OB_UIME", by.y="Obcina")) + tm_polygons("vrednost", title="Odstotki") + 
   tm_layout(title="Stopnja registrirane brezposelnosti (v %) po občinah v Sloveniji v letu 2015",legend.position=c(0.82,0.005), legend.height=0.7, title.size=1.5) 
-z3
 
 #stevilo stanovanj na 1000 prebivalcev
 z4 <- tm_shape(merge(obcine, stanovanja_z, by.x="OB_UIME", by.y="Obcina")) + tm_polygons("vrednost", title="Število stanovanj na 1000 preb.") + 
   tm_layout(title="Število stanovanj na 1000 prebivalcev v Sloveniji po občinah v letu 2015",legend.position=c(0.82,0.005), legend.height=0.7, title.size=1.5) 
-z4
 
 #LINEARNA REGRESIJA
 
 #linearen model med ogrevanjem in placami
-ggplot(t, aes(x = Place, y = delez)) + 
-  geom_point()
-# izračun modela
 fit <- lm(delez ~ Place, data=t)
 summary(fit)
-# izris modela
 m1 <- ggplot(t, aes(x = Place, y = t$delez)) + 
   geom_point() + 
   geom_smooth(method=lm, se=FALSE) + labs(x = "Plače glede na slovensko povprečje (indeks = 100)", y = "Delež stanovanj brez centralnega ogrevanja (v %)", fill = NULL, title = "Ponazoritev premice linearne regresije - vpliv plač na obseg napeljav centralnega ogrevanja")
-plot(m1)
 
 #linearen model med ogrevanjem in brezposelnostjo
-ggplot(t, aes(x = Brezposelni_ods, y = delez)) + 
-  geom_point()
-# izračun modela
 fit <- lm(delez ~ Brezposelni_ods , data=t)
 summary(fit)
-# izris modela
 m2 <- ggplot(t, aes(x = Brezposelni_ods, y = t$delez)) + 
   geom_point() + 
   geom_smooth(method=lm, se=FALSE) + labs(x = "Stopnja brezposelnosti (v %)", y = "Delež stanovanj brez centralnega ogrevanja (v %)", fill = NULL, title = "Ponazoritev premice linearne regresije - vpliv brezposelnosti na obseg napeljav centralnega ogrevanja")
-plot(m2)
