fe218b6f9e27dafa7e3ff256227fc093bd1a5ca7
Enja Erker <enja.erker@gmail.com>
2020-06-04 13:37:33 +0200

    popravki velikosti naslovov in legend grafov

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -40,7 +40,7 @@ place <- data.table(Višina, Število)
 
 t3 <- ggplot(place, aes(x="", y=Število, fill=Višina)) + geom_bar(stat="identity", width=1) +
   coord_polar("y", start=0) + scale_fill_manual(values=c("#2F4858", "#F6AE2D", "#999999")) +
-  labs(x = NULL, y = NULL, fill = NULL, title = "Povprečne mesečne neto plače glede na slovensko povprečje leta 2015") +
+  labs(x = NULL, y = NULL, fill = NULL, title = "Povprečne mesečne neto plače leta 2015") +
   theme_classic() + theme(axis.line = element_blank(),axis.text = element_blank(),axis.ticks = element_blank(),plot.title = element_text(hjust = 0.5, color = "#666666"))+
   geom_text(aes(label = paste(round(Število / sum(Število) * 100, 1), "%"), x = 1.3),position = position_stack(vjust = 0.5))
 
@@ -48,17 +48,17 @@ t3 <- ggplot(place, aes(x="", y=Število, fill=Višina)) + geom_bar(stat="identi
 
 #graficni prikaz vodovoda in gostote
 g1 <- ggplot(leta.skupine.vod) + aes(x=leto, y=povprecje, color=skupina) + geom_line() + geom_point() +
-  labs(x="Leto",y="Povprečno število stanovanj z vodovodom",title="Število vodovodnih napeljav glede na gostoto poselitve, 2002 - 2010", color = "Gostota poselitve") +
+  labs(x="Leto",y="Povprečno število stanovanj z vodovodom",title="Število vodovodnih napeljav glede na gostoto poselitve", color = "Gostota poselitve") +
   theme(legend.position = "bottom") 
 
 #graficni prikaz kanalizacije in gostote
 g2 <- ggplot(leta.skupine.kan) + aes(x=leto, y=povprecje, color=skupina) + geom_line() + geom_point() +
-  labs(x="Leto",y="Povprečno število stanovanj s kanalizacijo",title="Število napeljav kanalizacije glede na gostoto poselitve, 2002 - 2010", color = "Gostota poselitve") +
+  labs(x="Leto",y="Povprečno število stanovanj s kanalizacijo",title="Število napeljav kanalizacije glede na gostoto poselitve", color = "Gostota poselitve") +
   theme(legend.position = "bottom") 
 
 #graficni prikaz plac in ogrevanja
 g3 <- ggplot(leta.razredi) + aes(x=leto, y=povprecje, color=razred) + geom_line() + geom_point() +
-  labs(x="Leto",y="Povprečno število stanovanj z ogrevanjem",title="Število ogrevalnih napeljav in plače prebivalstva, 2002 - 2010", color = "Povprečna višina plač") +
+  labs(x="Leto",y="Povprečno število stanovanj z ogrevanjem",title="Število ogrevalnih napeljav in plače prebivalstva", color = "Povprečna višina plač") +
   theme(legend.position = "bottom") 
 
 #POMANKLJIVOSTI, TORTNI PRIKAZ
@@ -84,19 +84,19 @@ tr <- ggplot(pomankljivi, aes(x="", y=Število, fill=Stopnja)) + geom_bar(stat="
 proj4string(obcine)<- CRS("+proj=utm +zone=10+datum=WGS84")
 
 z1 <- tm_shape(merge(obcine, gostota_z, by.x="OB_UIME", by.y="Obcina")) + tm_polygons("vrednost", title="Gostota naseljenosti (preb/km^2)") +
-  tm_layout(title="Gostota prebivalstva Slovenije leta 2015", legend.position=c(0.65,0.005), legend.height=0.7, title.size=1.5)
+  tm_layout(title="Gostota prebivalstva Slovenije leta 2015", legend.position=c(0.65,0.005), legend.height=0.7, title.size=1.5, legend.title.size=0.2)
 
 #place
 z2 <- tm_shape(merge(obcine, placa_indeks_z, by.x="OB_UIME", by.y="Obcina")) + tm_polygons("vrednost", title="Vrednost indeksa") + 
-  tm_layout(title="Povprečne mesečne neto plače leta 2015",legend.position=c(0.82,0.005), legend.height=0.7, title.size=1.5) 
+  tm_layout(title="Povprečne mesečne neto plače leta 2015",legend.position=c(0.72,0.005), legend.height=0.7, title.size=1.5) 
 
 #brezposelni
 z3 <- tm_shape(merge(obcine, brezposelni_z, by.x="OB_UIME", by.y="Obcina")) + tm_polygons("vrednost", title="Odstotki") + 
-  tm_layout(title="Stopnja registrirane brezposelnosti v letu 2015",legend.position=c(0.82,0.005), legend.height=0.7, title.size=1.5) 
+  tm_layout(title="Stopnja reg. brezposelnosti v letu 2015",legend.position=c(0.82,0.005), legend.height=0.7, title.size=1.5) 
 
 #stevilo stanovanj na 1000 prebivalcev
 z4 <- tm_shape(merge(obcine, stanovanja_z, by.x="OB_UIME", by.y="Obcina")) + tm_polygons("vrednost", title="Število stanovanj na 1000 preb.") + 
-  tm_layout(title="Število stanovanj v letu 2015",legend.position=c(0.68,0.005), legend.height=0.7, title.size=1.5) 
+  tm_layout(title="Število stanovanj v letu 2015",legend.position=c(0.68,0.005), legend.height=0.7, title.size=1.5, legend.title.size=0.2) 
 
 #LINEARNA REGRESIJA
 
@@ -105,11 +105,11 @@ fit <- lm(delez ~ Place, data=t)
 summary(fit)
 m1 <- ggplot(t, aes(x = Place, y = t$delez)) + 
   geom_point() + 
-  geom_smooth(method=lm, se=FALSE) + labs(x = "Plače glede na slovensko povprečje (indeks = 100)", y = "Delež stanovanj brez centralnega ogrevanja (v %)", fill = NULL, title = "Ponazoritev premice linearne regresije - vpliv plač na centralno ogrevanje")
+  geom_smooth(method=lm, se=FALSE) + labs(x = "Plače glede na slovensko povprečje (indeks = 100)", y = "Delež stanovanj brez centralnega ogrevanja (v %)", fill = NULL, title = "Ponazoritev premice linearne regresije - \n vpliv plač na centralno ogrevanje")
 
 #linearen model med ogrevanjem in brezposelnostjo
 fit <- lm(delez ~ Brezposelni_ods , data=t)
 summary(fit)
 m2 <- ggplot(t, aes(x = Brezposelni_ods, y = t$delez)) + 
   geom_point() + 
-  geom_smooth(method=lm, se=FALSE) + labs(x = "Stopnja brezposelnosti (v %)", y = "Delež stanovanj brez centralnega ogrevanja (v %)", fill = NULL, title = "Ponazoritev premice linearne regresije - vpliv brezposelnosti na centralno ogrevanje")
+  geom_smooth(method=lm, se=FALSE) + labs(x = "Stopnja brezposelnosti (v %)", y = "Delež stanovanj brez centralnega ogrevanja (v %)", fill = NULL, title = "Ponazoritev premice linearne regresije - \n vpliv brezposelnosti na centralno ogrevanje")
